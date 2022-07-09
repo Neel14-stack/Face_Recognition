c5448779b0aacf30c88fd3fe3fc0819e79b0b3d4
Neel Madhab <54886608+Neel14-stack@users.noreply.github.com>
2022-07-09 23:27:22 +0530

    updated

--- face-detection-with-python-source-code/embeddings.py
+++ face-detection-with-python-source-code/embeddings.py
@@ -1,12 +1,11 @@
 import sys
 
-#sys.path.append('/home/shivam/Downloads/lib/python3.6/site-packages')
 sys.path.append('/home/shivam/Downloads/face-recognition/lib/python3.6/site-packages')
 import cv2 
 import face_recognition
 import pickle
-name=input("enter name")
-ref_id=input("enter id")
+name=input("enter name: ")
+ref_id=input("enter id: ")
 
 try:
 	f=open("ref_name.pkl","rb")
@@ -40,8 +39,6 @@ for i in range(5):
 	while True:
 	     
 		check, frame = webcam.read()
-		# print(check) #prints true as long as the webcam is running
-		# print(frame) #prints matrix values of each framecd 
 		cv2.imshow("Capturing", frame)
 		small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 		rgb_small_frame = small_frame[:, :, ::-1]
@@ -52,19 +49,12 @@ for i in range(5):
 			face_locations = face_recognition.face_locations(rgb_small_frame)
 			if face_locations != []:
 
-				# filename="photo.jpg"
-				# cv2.imwrite(filename=filename, img=frame)
-				# image = face_recognition.load_image_file(filename)
-				# image = Image.fromarray(frame)
-				# image = image.convert('RGB')
 				face_encoding = face_recognition.face_encodings(frame)[0]
 				if ref_id in embed_dictt:
 					embed_dictt[ref_id]+=[face_encoding]
 				else:
 					embed_dictt[ref_id]=[face_encoding]
 				webcam.release()
-				# img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
-				# img_new = cv2.imshow("Captured Image", img_new)
 				cv2.waitKey(1)
 				cv2.destroyAllWindows()     
 				break
